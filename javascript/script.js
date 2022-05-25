361e90baaee2211ef9426252ffc238acaaa350e5
YenniferUlloa04 <yenniferulloa04@gmail.com>
2022-05-24 20:37:00 -0500

    actividad 4 unidad 2

---
+++ javascript/script.js
@@ -0,0 +1,97 @@
+$(".bloquetitulo1").css("color","white")
+
+$(".bloquetitulo1").css("font-size", "50px")
+
+$(".promedio").css("color","white")
+
+$(".promedio").css("font-size", "50px")
+
+$(".superior").mouseover(function(){
+
+    $(".superior").css("background", "pink")
+
+})
+
+$(".superior").mouseout(function(){
+
+    $(".superior").css("background", "white")
+
+})
+
+
+$(".superior").click(function(){
+
+    $(".superior").css("font-size", "20px")
+  })
+
+
+  $(".alto").mouseover(function(){
+
+    $(".alto").css("background", "blue")
+
+})
+
+$(".alto").mouseout(function(){
+
+    $(".alto").css("background", "white")
+
+})
+
+
+$(".alto").dblclick(function(){
+
+    $(".alto").css("font-size", "20px")
+  })
+
+  $(".medio").mouseover(function(){
+
+    $(".medio").css("background", "green")
+
+})
+
+$(".medio").mouseout(function(){
+
+    $(".medio").css("background", "white")
+
+})
+
+$(".medio").click(function(){
+
+    $(".medio").css("font-size", "20px")
+  })
+
+
+  $(".bajo").mouseover(function(){
+
+    $(".bajo").css("background", "yellow")
+
+})
+
+$(".bajo").mouseout(function(){
+
+    $(".bajo").css("background", "white")
+
+})
+
+$(".bajo").dblclick(function(){
+
+    $(".bajo").css("font-size", "20px")
+  })
+
+
+  $(".superbajo").click(function(){
+
+    $(".superbajo").css("font-size", "20px")
+  })
+
+  $(".superbajo").mouseover(function(){
+
+    $(".superbajo").css("background", "orange")
+
+})
+
+$(".superbajo").mouseout(function(){
+
+    $(".superbajo").css("background", "white")
+
+})
